e9c860620fbbf29fb0530fbe99250b3491aa74d4
upce <u202112423@upc.edu.pe>
2026-09-21 17:45:42 -0500

    feat(home): add home vue home

---
+++ src/shared/presentation/views/home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+
+const {t} = useI18n();
+</script>
+
+<template>
+<section class="pt-6 p-4 md:p-5">
+  <div class="flex flex-col gap-3">
+    <h1 class="text-2xl font-bold">{{ t('home.title') }}</h1>
+  </div>
+</section>
+</template>
+
