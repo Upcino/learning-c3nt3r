35195d78fbc687997694ba6b124a7851364088e4
upce <u202112423@upc.edu.pe>
2026-09-21 18:50:03 -0500

    add all

---
+++ src/shared/presentation/components/footer.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+import {useRoute} from "vue-router";
+
+const {t} = useI18n();
+const route = useRoute();
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import{useI18n} from "vue-i18n";
+import {ref} from "vue";
+
+const {t} = useI18n();
+const drawer = ref(false);
+
+</script>
+
+<template>
+
+</template>
+
+
+<pv-toast/>
+<pv-confirm-dialog/>
+<header
+<style scoped>
+
+</style>
